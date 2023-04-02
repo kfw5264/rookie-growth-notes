5b1d385e43fde99442c3ddb1a6894cc432d62de5
masq <kfw5264@163.com>
2023-04-02 17:02:21 +0800

    希尔排序

--- algorithm/src/com/masq/sort/ShellSort.java
+++ algorithm/src/com/masq/sort/ShellSort.java
@@ -1,5 +1,7 @@
 package com.masq.sort;
 
+import java.util.Arrays;
+
 /**
  * 希尔排序
  * @author masq
@@ -7,7 +9,8 @@ package com.masq.sort;
 public class ShellSort {
 
     public static void main(String[] args) {
-
+        int[] array = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        System.out.println(Arrays.toString(shellSort(array)));
     }
 
     private static int[] shellSort(int[] array) {
@@ -15,7 +18,19 @@ public class ShellSort {
         int temp, gap = len / 2;
 
         while(gap > 0) {
+            for (int i = gap; i < len; i++) {
+                temp = array[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && temp < array[preIndex]) {
+                    array[preIndex + gap] = array[preIndex];
+                    preIndex -= gap;
+                }
+                array[preIndex + gap] = temp;
+            }
 
+            gap = gap / 2;
         }
+        return array;
     }
+
 }
